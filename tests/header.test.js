3dc0ceaf383afa4dafa1509c94aaa65a9870b9c9
Dhvanit41 <50017804+Dhvanit41@users.noreply.github.com>
2021-06-30 23:11:48 +0530

    chrome puppeteer opens browser with new page

---
+++ tests/header.test.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+test('Add to number ',()=>{
+    const sum = 1+3;
+    expect(sum).toEqual(4)
+})
+
+
+test('We can launch a browser',async()=>{
+    const browser = await puppeteer.launch({
+        headless:false
+    });
+    const page = await browser.newPage();
+
+})
