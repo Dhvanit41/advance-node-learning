972082773edb2e77af979085c4274a2270168ab0
Dhvanit41 <50017804+Dhvanit41@users.noreply.github.com>
2021-07-17 23:41:49 +0530

    travis ci setup

--- config/dev.js
+++ config/dev.js
@@ -4,4 +4,5 @@ module.exports = {
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: 'mongodb://advance-node:8va8TLL5uor6h0rA@cluster0-shard-00-00.4ap3q.mongodb.net:27017,cluster0-shard-00-01.4ap3q.mongodb.net:27017,cluster0-shard-00-02.4ap3q.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-g7emeq-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
+  redisUrl:"redis://127.0.0.1:6379"
 };
--- tests/header.test.js
+++ tests/header.test.js
@@ -14,9 +14,15 @@ afterEach(async ()=>{
     await browser.close();
 })
 
-test('We can launch a browser',async()=>{
+test('Header has the correct text',async()=>{
     const text= await page.$eval('a.brand-logo',el=>el.innerHTML);
-    expect(text).toEqual('Blogster');
-
-    
+    expect(text).toEqual('Blogster');    
 })
+
+test('Cliking login starts o-auth flow',async()=>{
+    await page.click('.right a');
+    const url = await page.url();
+    expect(url).toMatch(/accounts\.google\.com/);
+})
+
+
